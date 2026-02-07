a7148cfabe214f4db7bf04d8d98ced6114676868
Tickles <jack@gtctek.co.uk>
2026-02-07 21:07:41 +0000

    Update backend.js

--- backend.js
+++ backend.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 
 app.use(cors()); // allow all origins (quick fix)
 
-const DATA_PATH = path.join(__dirname, "Data.json");
+const DATA_PATH = path.join(__dirname, "Data.Json");
 
 app.get("/Data.json", (req, res) => {
   const data = require(DATA_PATH); // parses JSON automatically res.json(data)
@@ -82,6 +82,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server started on", PORT);
 });
+
 
 
 
