00e2ce1429cf383be11389b1e408c58e65b7747a
Tickles <jack@gtctek.co.uk>
2026-02-16 23:32:42 +0000

    Update backend.js

--- backend.js
+++ backend.js
@@ -53,7 +53,9 @@ app.get("/TEST",async(req,res) => {
 
   
 
-app.get("/NAME", (req, res) => {
+app.get("/NAME", (req, res) => {#
+    console.log(req.query.user)
+    console.log(req.query.data)
     overwrite(req.query.user,req.query.data,"username")
     res.send("Done")
 })
