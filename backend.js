24619f8f73ad2fa6b39eb4fd02963da8732a7dc2
Tickles <jack@gtctek.co.uk>
2026-02-16 14:22:55 +0000

    Change GitHub token in backend.js
    Updated GitHub token for authentication.

--- backend.js
+++ backend.js
@@ -113,7 +113,7 @@ app.listen(PORT, "0.0.0.0", () => {
   console.log("Server started on", PORT);
 });
 
-const token = "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"
+const token = "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"
 const owner = "ewewsheep"
 const repo = "Cap11.co.uk"
 const pathtd = "Data.Json"
