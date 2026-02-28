0b548eb1db1348d2f6f7934f8c0c2743dbd26f71
Tickles <jack@gtctek.co.uk>
2026-02-28 23:51:35 +0000

    Update backend.js

--- backend.js
+++ backend.js
@@ -11,9 +11,13 @@ app.use(cors()); // allow all origins (quick fix)
 const DATA_PATH = path.join(__dirname, "Dataa.Json");
 const WEB_PATH = "https://raw.githubusercontent.com/ewewsheep/Cap11.co.uk/refs/heads/main/Dataa.Json";
 
-const response = await fetch(WEB_PATH);
-const text = await response.text();
-await f.writeFile(DATA_PATH, text, "utf8"); 
+async function syncFile() {
+  const response = await fetch(WEB_PATH);
+  const text = await response.text();
+  await f.writeFile(DATA_PATH, text, "utf8");
+}
+
+SyncFile()
 
 
 app.get("/Dataa.json", async (req, res) => {
