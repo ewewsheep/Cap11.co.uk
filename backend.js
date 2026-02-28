5c8883da5997b9eb10180d98503fcdbab86e31ce
Tickles <jack@gtctek.co.uk>
2026-02-28 21:30:02 +0000

    Update backend.js

--- backend.js
+++ backend.js
@@ -75,7 +75,7 @@ async function overwriteB(a,b,c){
         z[c] = b.trim();
         console.log("bothRequal");
     }
-    fs.writeFile("/Dataa.Json", JSON.stringify(tfile), "utf8");
+    fs.writeFile("/Dataa.Json", JSON.stringify(tfile));
     console.log(z.username);
   });
   console.log("GitHub response:", ghJson);
