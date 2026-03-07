57619b263e5f0ff1cc87f098931e6ed55fa9f234
Tickles <jack@gtctek.co.uk>
2026-03-07 15:58:45 +0000

    Update backend.js

--- backend.js
+++ backend.js
@@ -46,12 +46,13 @@ app.get("/TEST",async(req,res) => {
 
 app.get("/YES", async (req, res) => {
     const to = "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"
-    const og = await fetch("cap11.co.uk/System.Json");
+    const og = await fetch("https://cap11.co.uk/System.Json");
     const jso = await og.json();
+    console.log("Part1")
     jso.forEach(item => {
       if(item.name == "vote") item.yes = item.yes + 1;
     });
-
+    console.log("Part2")
     const fileRes = await fetch("https://api.github.com/repos/ewewsheep/Cap11.co.uk/contents/System.Json",{headers:{"Authorization": `Bearer ${to}`}})
     
     const fileData = await fileRes.json();
@@ -69,6 +70,7 @@ app.get("/YES", async (req, res) => {
         content: encoded,
         sha: sha
       })})
+      console.log("Part3")
       res.send("YES VOTED")
     });
   
