7ff2156ea989a9be16cfc526c67dff9b21acc214
Tickles <jack@gtctek.co.uk>
2026-04-12 10:50:41 +0100

    Change JSON parsing method in backend.js
    Updated JSON parsing method from data.json() to JSON.parse().

--- backend.js
+++ backend.js
@@ -13,13 +13,9 @@ const SYSTEM_PATH = path.join(__dirname, "System.Json");
 
 app.get("/Data.json", async (req, res) => {
   const data =  await fetch(WEB_PATH);
-  const json = await data.json();// parse string into JS object
+  const json = await JSON.parse(data);// parse string into JS object
   const json2 = Object.values(json)
-  console.log(json2)////////////////////////////////////////////
-  const text = await data.text();
-console.log(typeof text);
-console.log(text);
-  res.json(json2);//////////////////////////////////////////////
+  console.log(json2)
 });
 
 app.get("/System.Json", async (req, res) => {
