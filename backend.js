87ae96c189f51301faa285b251db87a9911da192
Tickles <jack@gtctek.co.uk>
2026-06-03 19:43:18 +0100

    Fix array push to parse query parameter

--- backend.js
+++ backend.js
@@ -61,7 +61,7 @@ app.get("/NEW", async (req, res) => {
     var file = await fetch("https://cap11-data-default-rtdb.europe-west1.firebasedatabase.app/v/vote.json")
       var tfile = await file.json()
       a = Object.values(tfile)
-      b = a.push(req.array)
+      b = a.push(JSON.parse(req.query.array))
       await fetch("https://cap11-data-default-rtdb.europe-west1.firebasedatabase.app/v/vote.json",{method:"PUT",headers:{"Content-Type":"application/json"},body:JSON.stringify(b)})
       res.send("profile added")
     });
