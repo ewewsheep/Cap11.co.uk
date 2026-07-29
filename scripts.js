a26fb94d5724daad6982d8516eefa24048a802fb
Tickles <jack@gtctek.co.uk>
2026-07-29 17:28:26 +0100

    Refactor cookie handling and user profile functions

--- scripts.js
+++ scripts.js
@@ -137,7 +137,7 @@ async function cChat(){
         var message = document.getElementById("msbox").value
         var rank = GetInfo("rankn")
         var name = GetInfo("username")
-        fetch("/ADDMESS?array=" + encodeURIComponent(JSON.stringify({"name":`${name}`,"rank":`${rank}`,"message":`${message}`,"id":`${count}` })))
+        await fetch("/ADDMESS?array=" + encodeURIComponent(JSON.stringify({"name":`${name}`,"rank":`${rank}`,"message":`${message}`,"id":`${count}` })))
       }
 
 async function addclub(club){
